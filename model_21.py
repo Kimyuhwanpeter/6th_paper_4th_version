a2e9f0f97cc34ab607c064c52f14204e16b257c5
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-06-09 20:25:35 +0900

    Create model_21.py

---
+++ model_21.py
@@ -0,0 +1,315 @@
+# -*- coding:utf-8 -*-
+from model_profiler import model_profiler
+import tensorflow as tf
+
+def three_decoder_Unet(input_shape=(384, 384, 3), nclasses=1):
+
+    h = inputs = tf.keras.Input(input_shape)
+
+    h = tf.keras.layers.Conv2D(filters=96, kernel_size=3, padding="same", use_bias=False, groups=3)(h)
+    
+    h_1 = h[:, :, :, 0:32]
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_2 = h[:, :, :, 32:64]
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_3 = h[:, :, :, 64:]
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+
+    h_1 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    block_1_1 = h_1
+
+    h_2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    block_1_2 = h_2
+
+    h_3 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    block_1_3 = h_3
+
+    h = tf.concat([h_1, h_2, h_3], -1)
+    h = tf.keras.layers.MaxPool2D((2,2), 2)(h)
+    h = tf.keras.layers.Conv2D(filters=192, kernel_size=3, padding="same", use_bias=False, groups=3)(h)
+
+    h_1 = h[:, :, :, 0:64]
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_2 = h[:, :, :, 64:128]
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_3 = h[:, :, :, 128:]
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+
+    h_1 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    block_2_1 = h_1
+
+    h_2 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    block_2_2 = h_2
+
+    h_3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    block_2_3 = h_3
+
+    h = tf.concat([h_1, h_2, h_3], -1)
+    h = tf.keras.layers.MaxPool2D((2,2), 2)(h)
+    h = tf.keras.layers.Conv2D(filters=384, kernel_size=3, padding="same", use_bias=False, groups=3)(h)
+
+    h_1 = h[:, :, :, 0:128]
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_2 = h[:, :, :, 128:256]
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_3 = h[:, :, :, 256:]
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+
+    h_1 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    block_3_1 = h_1
+
+    h_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    block_3_2 = h_2
+
+    h_3 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    block_3_3 = h_3
+
+    h = tf.concat([h_1, h_2, h_3], -1)
+    h = tf.keras.layers.MaxPool2D((2,2), 2)(h)
+    h = tf.keras.layers.Conv2D(filters=768, kernel_size=3, padding="same", use_bias=False, groups=3)(h)
+
+    h_1 = h[:, :, :, 0:256]
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_2 = h[:, :, :, 256:512]
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_3 = h[:, :, :, 512:]
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+
+    h_1 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+
+    h_2 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+
+    h_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+
+    ####################################################################################################
+    #
+    h = tf.concat([h_1, h_2, h_3], -1)
+    h = tf.keras.layers.Conv2D(filters=768, kernel_size=3, padding="same", use_bias=False, groups=3)(h)
+
+    h_1 = h[:, :, :, 0:256]
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_2 = h[:, :, :, 256:512]
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_3 = h[:, :, :, 512:]
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+
+    h_1 = tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=2, strides=2)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1_att = h_1
+    h_1 = tf.concat([block_3_1, h_1], -1)
+
+    h_2 = tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=2, strides=2)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2_att = h_2
+    h_2 = tf.concat([block_3_2, h_2], -1)
+
+    h_3 = tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=2, strides=2)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3_att = h_3
+    h_3 = tf.concat([block_3_3, h_3], -1)
+
+    #
+    h = tf.concat([h_1, h_2, h_3], -1)
+    h = tf.keras.layers.Conv2D(filters=384, kernel_size=3, padding="same", use_bias=False, groups=3)(h)
+
+    h_1 = h[:, :, :, 0:128]
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = h_1_att + h_1
+    h_2 = h[:, :, :, 128:256]
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = h_2_att + h_2
+    h_3 = h[:, :, :, 256:]
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = h_3_att + h_3
+
+    h_1 = tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=2, strides=2)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1_att = h_1
+    h_1 = tf.concat([block_2_1, h_1], -1)
+
+    h_2 = tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=2, strides=2)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2_att = h_2
+    h_2 = tf.concat([block_2_2, h_2], -1)
+
+    h_3 = tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=2, strides=2)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3_att = h_3
+    h_3 = tf.concat([block_2_3, h_3], -1)
+    #
+    h = tf.concat([h_1, h_2, h_3], -1)
+    h = tf.keras.layers.Conv2D(filters=192, kernel_size=3, padding="same", use_bias=False, groups=3)(h)
+
+    h_1 = h[:, :, :, 0:64]
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = h_1_att + h_1
+    h_2 = h[:, :, :, 64:128]
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = h_2_att + h_2
+    h_3 = h[:, :, :, 128:]
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = h_3_att + h_3
+
+    h_1 = tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=2, strides=2)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1_att = h_1
+    h_1 = tf.concat([block_1_1, h_1], -1)
+    h_1 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", use_bias=False)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+    h_1 = h_1_att + h_1
+    h_1 = tf.keras.layers.Conv2D(filters=nclasses, kernel_size=1)(h_1)
+    h_1 = tf.keras.layers.BatchNormalization()(h_1)
+    h_1 = tf.keras.layers.ReLU()(h_1)
+
+    h_2 = tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=2, strides=2)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2_att = h_2
+    h_2 = tf.concat([block_1_2, h_2], -1)
+    h_2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", use_bias=False)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+    h_2 = h_2_att + h_2
+    h_2 = tf.keras.layers.Conv2D(filters=nclasses, kernel_size=1)(h_2)
+    h_2 = tf.keras.layers.BatchNormalization()(h_2)
+    h_2 = tf.keras.layers.ReLU()(h_2)
+
+    h_3 = tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=2, strides=2)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3_att = h_3
+    h_3 = tf.concat([block_1_3, h_3], -1)
+    h_3 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", use_bias=False)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+    h_3 = h_3_att + h_3
+    h_3 = tf.keras.layers.Conv2D(filters=nclasses, kernel_size=1)(h_3)
+    h_3 = tf.keras.layers.BatchNormalization()(h_3)
+    h_3 = tf.keras.layers.ReLU()(h_3)
+
+    return tf.keras.Model(inputs=inputs, outputs=[h_1, h_2, h_3])
